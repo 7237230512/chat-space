4bee8cc624c5ab62f6d0b6c55d13b5309bb0faa6
7237230512 <56635909+7237230512@users.noreply.github.com>
2019-10-29 14:56:01 +0900

    message機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
 end
