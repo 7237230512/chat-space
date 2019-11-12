7edafa204da6fa780bdf9896f08890b750b8d28c
7237230512 <56635909+7237230512@users.noreply.github.com>
2019-11-12 17:30:48 +0900

    デプロイ

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,9 +6,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
 
 
-  process resize_to_fit: [300, 300]
-  storage :file
-  # storage :fog
+  # Choose what kind of storage to use for this uploader:
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
